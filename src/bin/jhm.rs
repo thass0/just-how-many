a0e704514ed45679078913883411b3af811b2d67
Thassilo Schulze <d4kd@proton.me>
2023-12-03 20:11:15 +0100

    Add generate command to the CLI

--- src/bin/jhm.rs
+++ src/bin/jhm.rs
@@ -1,8 +1,7 @@
-use std::path::PathBuf;
-
 use clap::{Parser, Subcommand};
 use url::Url;
 use anyhow::Context;
+use uuid::Uuid;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -15,10 +14,12 @@ struct Cli {
 enum Commands {
     /// Get the number of hits that a page has.
     Hits {
+	/// Page to get the number of hits of.
 	url: Url,
     },
-    /// Register a URL to count page hits on.
-    Register {
+    /// Generate the CSS that's needed to track a page.
+    Generate {
+	/// URL of the page to track.
 	url: Url,
     },
 }
@@ -29,10 +30,11 @@ const ADDRESS: &str = "http://localhost:8080"; // TODO: Get from envvar.
 
 fn get_hits(client: &reqwest::blocking::Client, url: &Url) -> anyhow::Result<i32> {
     let response = client.
-	get(format!("{}/hits", ADDRESS))
+	get(format!("{ADDRESS}/hits"))
 	.query(&[("url", url.as_str())])
 	.send()
 	.context("reqwest GET failed")?;
+
     if response.status().is_success() {
 	response
 	    .json::<i32>()
@@ -41,7 +43,27 @@ fn get_hits(client: &reqwest::blocking::Client, url: &Url) -> anyhow::Result<i32
 	let err = response
 	    .text()
 	    .context("Failed to receive error message")?;
-	Err(anyhow::anyhow!("Server error: {err}"))
+	anyhow::bail!("Server error: {err}")
+    }
+}
+
+fn post_register(client: &reqwest::blocking::Client, url: &Url) -> anyhow::Result<Uuid> {
+    let body = format!("url={url}");
+    let response = client
+        .post(&format!("{ADDRESS}/register"))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .context("reqwest POST failed")?;
+
+    if response.status().is_success() {
+	response.json::<Uuid>()
+	    .context("Failed to decode page ID")
+    } else {
+	let err = response
+	    .text()
+	    .context("Failed to receive error message")?;
+	anyhow::bail!("Server error: {err}")
     }
 }
 
@@ -60,6 +82,18 @@ fn main() {
 	    let s = if hits == 1 { "" } else { "s" };
 	    println!("🌟 {url} has {hits} hit{s}!");
 	},
-	Register { url } => println!("Register {url}"),
+	Generate { url } => {
+	    let page_id = post_register(&client, &url)
+		.expect(&format!("Failed to register {url}"));
+	    println!(r#"🗃️ SUCCESS! {url} is registered under the page ID {page_id}.
+This ID is used to track your page.
+
+Just put the following CSS the in style sheets of the page to track, and you're done!
+
+  body:hover {{
+      border-image: url("{ADDRESS}/hit/{page_id}");
+      border-width: 0;
+  }}"#);
+	},
     }
 }
